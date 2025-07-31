0f0015870a936aa06dab855efdd2699958657d0b
sureka-mariyan <surekaeddy@gmail.com>
2025-07-31 10:10:50 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿namespace playwrightTestSurabhi;
+
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+public class Tests
+{
+    // [SetUp]
+    // public void Setup()
+    // {
+    // }
+
+    [Test]
+    public async Task Test1()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false }); //lauch browser
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://www.google.co.in");
+        await page.PauseAsync();
+        var page1 = await browser.NewPageAsync();
+        await page1.GotoAsync("https://yahoo.com");
+        await page1.PauseAsync();
+    }
+}
